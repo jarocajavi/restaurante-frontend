4d63b6d6e7cf3bb2038bdec57befd2614c248d81
jarocajavi <jarocajavi@gmail.com>
2026-03-13 00:05:04 +0100

    Fix React Router con HashRouter para GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import RestaurantDetail from './pages/RestaurantDetail'
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/restaurant/:id" element={<RestaurantDetail />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
