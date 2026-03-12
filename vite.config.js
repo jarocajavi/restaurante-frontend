3a4363503e17724ff230fd76f092d61817181319
jarocajavi <jarocajavi@gmail.com>
2026-03-12 23:45:27 +0100

    Configuración de despliegue en Github Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/restaurante-frontend/',
   plugins: [
     react(),
     tailwindcss(),
